49e5a550e4f762272654727c53ddddb5eb55e2f3
Dongmin Yu <ydm2790@gmail.com>
2026-01-05 21:02:40 +0900

    chore: initial commit

---
+++ lib/src/clarity_flutter.dart
@@ -0,0 +1,7 @@
+/// {@template clarity_flutter}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class ClarityFlutter {
+  /// {@macro clarity_flutter}
+  const ClarityFlutter();
+}
