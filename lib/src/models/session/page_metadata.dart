344d36de6545eabf9f619752926003a5ba621674
Dongmin Yu <ydm2790@gmail.com>
2026-01-05 22:55:54 +0900

    feat: introduces session, page, and payload metadata models
    Establishes the foundational data structures required for tracking user sessions and their associated interactions.
    
    `SessionMetadata` captures essential details of a user session. `PageMetadata` provides context for individual page views, linking them to a session. `PayloadMetadata` defines the structure for discrete data chunks within a page, including timing and sequencing information.
    
    This framework is crucial for data collection, serialization, and processing in the analytics pipeline.

---
+++ lib/src/models/session/page_metadata.dart
@@ -0,0 +1,29 @@
+/// Copyright (c) Microsoft Corporation.
+/// Licensed under the MIT License.
+library;
+
+import 'package:json_annotation/json_annotation.dart';
+
+import 'session_metadata.dart';
+
+part '../generated/page_metadata.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class PageMetadata {
+  PageMetadata(this.number, this.startTime, this.lastVisibilityEventState, this.screenName, this.session);
+
+  factory PageMetadata.fromJson(Map<String, dynamic> json) => _$PageMetadataFromJson(json);
+
+  int number;
+  int startTime;
+  String lastVisibilityEventState;
+  String screenName;
+  SessionMetadata session;
+
+  @override
+  String toString() {
+    return 'PageMetadata(number: $number, startTime: $startTime, lastVisibilityEventState: $lastVisibilityEventState, screenName: $screenName, session: $session)';
+  }
+
+  Map<String, dynamic> toJson() => _$PageMetadataToJson(this);
+}
---
+++ lib/src/models/session/session_metadata.dart
@@ -0,0 +1,28 @@
+/// Copyright (c) Microsoft Corporation.
+/// Licensed under the MIT License.
+library;
+
+import 'package:json_annotation/json_annotation.dart';
+
+part '../generated/session_metadata.g.dart';
+
+@JsonSerializable(explicitToJson: true)
+class SessionMetadata {
+  SessionMetadata(this.startTime, this.id, this.projectId, this.userId, this.ingestUrl, this.version);
+
+  factory SessionMetadata.fromJson(Map<String, dynamic> json) => _$SessionMetadataFromJson(json);
+
+  int startTime;
+  String id;
+  String projectId;
+  String userId;
+  String ingestUrl;
+  String version;
+
+  @override
+  String toString() {
+    return 'SessionMetadata(sessionId: $id, startTime: $startTime, projectId: $projectId, userId: $userId, ingestUrl: $ingestUrl, version: $version)';
+  }
+
+  Map<String, dynamic> toJson() => _$SessionMetadataToJson(this);
+}
